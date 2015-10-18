d9f4fcef6953d3c71c94886274f5a135c697014b
Tim 'mithro' Ansell <mithro@mithis.com>
2015-10-18 18:05:10 +1100

    Compatibility definitions.

---
+++ compat-linux-types.h
@@ -0,0 +1,24 @@
+// These defines are used to make Linux Kernel headers compatible with SDCC
+
+#ifndef COMPAT_LINUX_TYPES_H
+#define COMPAT_LINUX_TYPES_H
+
+#include <fx2types.h>
+
+/* The 8051 is an 8bit processor, so doesn't actually have any endian
+ * naturally, instead the endian comes from the compiler. 
+ *
+ * sdcc choses to be little endian, as does the USB specification.
+ */
+
+#define __u8    BYTE
+#define __le16  WORD
+#define __le32  DWORD
+
+#define __le16_to_cpu
+
+// Use a macro to make __attribute__ stuff disappear. sdcc always generates
+// "packed" structures.
+#define __attribute__()
+
+#endif // COMPAT_LINUX_TYPES_H
