27588deb9bffbbab48c5a692c9e4947f756730d8
Tim 'mithro' Ansell <mithro@mithis.com>
2015-10-30 21:11:12 +1100

    Adding cdc-extra.h (like uvc-extra.h)

---
+++ cdc-extra.h
@@ -0,0 +1,22 @@
+/*
+ * cdc-extra.h -- Extra definitions for use with include/uapi/linux/usb/cdc.h
+ *
+ * Copyright (C) 2015 Google, Inc.
+ *
+ * Tim 'mithro' Ansell <mithro@mithis.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _USB_CDC_EXTRA_H_
+#define _USB_CDC_EXTRA_H_
+
+#define CDC_BCD_V11 0x0110
+
+#endif
