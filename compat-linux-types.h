151790ec09e925d7d47189879384310cdf97b59b
Tim 'mithro' Ansell <mithro@mithis.com>
2015-10-30 21:09:01 +1100

    Adding defines so it can compile with GCC for easier debugging.

--- compat-linux-types.h
+++ compat-linux-types.h
@@ -3,7 +3,21 @@
 #ifndef COMPAT_LINUX_TYPES_H
 #define COMPAT_LINUX_TYPES_H
 
+#ifndef SDCC
+#include <stdint.h>
+
+#define BYTE int8_t
+#define WORD int16_t
+#define DWORD int32_t
+#define __xdata
+
+int main() {
+	return 0;
+}
+
+#else
 #include <fx2types.h>
+#endif
 
 /* The 8051 is an 8bit processor, so doesn't actually have any endian
  * naturally, instead the endian comes from the compiler. 
@@ -11,8 +25,11 @@
  * sdcc choses to be little endian, as does the USB specification.
  */
 
+
 #define __u8    BYTE
+#define __u16   __le16
 #define __le16  WORD
+#define __u32   __le32
 #define __le32  DWORD
 
 #define __le16_to_cpu
